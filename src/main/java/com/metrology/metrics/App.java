6780e9eb063f9b34f5f3f1893f352923d1df8ca9
casper <133casper@gmail.com>
2019-10-28 11:39:06 +0300

    Project skeleton

---
+++ src/main/java/com/metrology/metrics/App.java
@@ -0,0 +1,11 @@
+package com.metrology.metrics;
+
+public class App {
+    private String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
