01526853ee369d4dc2371d5cc204ad0b59693150
lo67 <astop_1310@mail.ru>
2019-10-31 19:29:34 +0300

    Удаление всего лишнего, кроме тел функций

--- src/main/java/com/metrology/metrics/model/LanguageAnalyzer.java
+++ src/main/java/com/metrology/metrics/model/LanguageAnalyzer.java
@@ -1,5 +1,6 @@
 package com.metrology.metrics.model;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LanguageAnalyzer {
@@ -14,6 +15,7 @@ public class LanguageAnalyzer {
         deleteAllComments();
         deleteControlCharacters();
         deleteAllTextLiterals();
+        keepFuncBodiesInCode();
     }
 
     private void deleteAllComments() {
@@ -46,6 +48,51 @@ public class LanguageAnalyzer {
 
         programCode = Pattern.compile(runeLiteralRegEx)
                 .matcher(programCode).replaceAll(" ");
+    }
+
+    private int countFuncBodySize(StringBuilder codeText) {
+        int curlyBracesCount = 1;
+        int charIndex = 1;
+
+        while (curlyBracesCount != 0) {
+            if (codeText.charAt(charIndex) == '{') {
+                curlyBracesCount++;       // или лучше ++curlyBracesCount? или не важно?
+            } else if (codeText.charAt(charIndex) == '}') {
+                curlyBracesCount--;
+            }
+
+            charIndex++;
+        }
+        return charIndex;
+    }
+
+    private void keepFuncBodiesInCode() {
+        int funcBodySize;
+        String funcBody;
+
+        StringBuilder codeText = new StringBuilder(programCode);
+        StringBuilder codeBuff = new StringBuilder(programCode.length());
+
+        Pattern funcPattern = Pattern.compile("(func [^{]*)");
+        Matcher funcMatcher = funcPattern.matcher(codeText.toString());
+        
+        /* Алгоритм:
+            1) Найти объявление функции
+            2) Убрать из текста символы до тела найденой функции
+            3) Посчитать размер тела функции
+            4) Добавить тело функции в буфер
+            5) Пока в тексте осталось объявление функции, повторять с шага 1
+            6) Перезаписать текст значением из буфера
+        */
+
+        while (funcMatcher.find()) {
+            codeText.delete(0, funcMatcher.start() + funcMatcher.group().length());
+            funcBodySize = countFuncBodySize(codeText);
+            funcBody = codeText.substring(0, funcBodySize);
+            codeBuff.append(funcBody);
+            funcMatcher = funcPattern.matcher(codeText.toString());
+        }
+        programCode = codeBuff.toString();
     }
 
     public String getProgramCode() {
